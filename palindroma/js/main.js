0dbf7f089501b7b966c9902e480b15d5c0512f57
Luca Somaschini <somas99@gmail.com>
2022-07-12 15:45:59 +0200

    Palindroma - Code working (no function yet)

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,1 +1,29 @@
-console.log('test');
+const userInput = prompt('Please, enter a word:');
+
+let prova1 = false;
+
+//debug
+console.log(userInput[0])
+console.log(userInput[1])
+console.log(userInput[2])
+console.log(userInput[3])
+
+for(let i = 0; i < userInput.length; i++) {
+    if (userInput[i] == userInput[userInput.length-i-1]) {
+        prova1 = true;
+        console.log(i); //debug
+        console.log(userInput[i]); //debug
+    } else if (userInput[i] != userInput[userInput.length-i]) {
+        prova1 = false;
+    }
+    console.log(prova1);//debug
+}
+
+
+if (prova1 == true) {
+    console.log('The chosen word is palindrome.');
+} else if (prova1 == false) {
+    console.log('The chosen word is NOT palindrome.');
+}
+
+console.log(userInput.length);
