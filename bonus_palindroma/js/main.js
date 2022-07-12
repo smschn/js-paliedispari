97731e1b2098dc338574ff11dd1c49b37c7c3e5e
Luca Somaschini <somas99@gmail.com>
2022-07-12 23:57:56 +0200

    Bonus_palindroma - Improved comments

--- bonus_palindroma/js/main.js
+++ bonus_palindroma/js/main.js
@@ -12,18 +12,18 @@ userInputReversedSplitted.reverse();
 console.log(userInputReversedSplitted);
 console.log(`The order of the array is reversed: ${userInputReversedSplitted}`);
 
-// the individual letters are united to form a new word
+// the individual letters are united to form a new word (the array does not exist anymore)
 let userInputReversed = userInputReversedSplitted.join('');
 console.log(userInputReversed);
 console.log(`All the letters are united to form a new word: ${userInputReversed}`);
 
-// create a variable to be flagged \ used inside the for loop
+// create a variable to be flagged \ used inside the for loop in the function
 let check;
 
 // create a function that checks if the user-entered word is palindrome
 function isPalindrome(functionParameter) {
 
-    // create a for loop to compare the two words
+    // create a for loop to compare the two words and to change the 'check' variable
     for (let i = 0; i < functionParameter.length; i++) {
         if (functionParameter[i] == userInputReversed[i]) {
             check = true;
@@ -36,10 +36,10 @@ function isPalindrome(functionParameter) {
     return check;
 }
 
-// create a variable to use the function with the userInput argument instead of the functionParameter parameter (which is a general rule)
+// create a variable to use the function with the 'userInput' argument instead of the 'functionParameter' parameter (which is used as a general rule to show how the function works)
 const isUserInputPalindrome = isPalindrome(userInput);
 
-// show the result with a conditional statement
+// show the result using a conditional statement
 if (check == true) {
     console.log(`The word ${userInput} is palindrome`);
 } else {
