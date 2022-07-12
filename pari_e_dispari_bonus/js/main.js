aa620fb8eec4cd53844ca6a089f20a690a41d0c7
Luca Somaschini <somas99@gmail.com>
2022-07-13 01:26:57 +0200

    Bonus_pari: Added function, improved code & comments

--- pari_e_dispari_bonus/js/main.js
+++ pari_e_dispari_bonus/js/main.js
@@ -1,33 +1,9 @@
-/*
 // user choice
 const userChoice = prompt('Choose: odd or even?');
 
 // user input
 const userNumber = parseInt(prompt('Please, choose a number between 1 and 5.'));
 
-let min;
-let max;
-*/
-
-let minNum = 1;
-let maxNum = 5;
-
-/*
-let randomNumber1 = Math.floor(Math.random() * maxNum + minNum);
-console.log(randomNumber)
- */
-
-function generateRandomNumber(numberMinimum, numberMaximum) {
-
-    const randomNumberFunction = Math.floor(Math.random() * (numberMaximum - numberMinimum + 1) + numberMinimum);
-    return randomNumberFunction;
-}
-
-const randomNumber = generateRandomNumber(minNum, maxNum);
-console.log(randomNumber);
-
-
-/*
 // check if the number chosen by the user is correct
 if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
 
@@ -38,22 +14,29 @@ if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
 
 } else {
 
-    // show in the console the user number
-    console.log('The human number is: ' + userNumber);
+    // show in the console the user choice
+    console.log(`The human choice is: ${userChoice}`);
 
-    // create a function to generate a random number between 1 and 5
-    function generateComputerNumber(minNum, maxNum) {
-        let randomNumber = Math.floor(Math.random() * maxNum + minNum);
-        return randomNumber;
+    // show in the console the user number
+    console.log(`The human number is: ${userNumber}`);
+
+    // create variables for the computer minimum and maximum numbers
+    let computerMinNum = 1;
+    let computerMaxNum = 5;
+
+    // create function to generate a casual number between a maximum and a minimum
+    function generateRandomNumber(numberMinimum, numberMaximum) {
+        const randomNumberFunction = Math.floor(Math.random() * (numberMaximum - numberMinimum + 1) + numberMinimum);
+        return randomNumberFunction;
     }
 
-    // create a variable to use the function && show the generated number in console
-    const computerNumber = generateComputerNumber();
-    console.log('The computer number is: ' + computerNumber);
+    // generate random number for the computer
+    const computerRandomNumber = generateRandomNumber(computerMinNum, computerMaxNum);
+    console.log(`The computer number is: ${computerRandomNumber}`);
 
     // create a variable to define the sum of the two numbers && show the sum in console
-    const sum = userNumber + computerNumber;
-    console.log('The sum of the two numbers is: ' + sum);
+    const sum = userNumber + computerRandomNumber;
+    console.log(`The sum of the two numbers is: ${sum}`);
 
     // create a variable \ flag to check if the sum is an odd or even number (it will be used in the next function)
     let isSumEven;
@@ -74,16 +57,15 @@ if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
     // check on 'result' if sum is even or odd
     if (result == true) {
         console.log('The sum of the two numbers is: even');
-    } else if (result == false) {
+    } else {
         console.log('The sum of the two numbers is: odd');
     }
 
     // check the winner
     if ((userChoice == 'even' && result == true) || (userChoice == 'odd' && result == false)) {
         console.log('Human wins!');
-    } else if ((userChoice == 'even' && result == false) || (userChoice == 'odd' && result == true)) {
+    } else {
         console.log('Computer wins!');
     }
 
 }
-*/
