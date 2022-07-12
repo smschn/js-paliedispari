3aba7d49004c6e8697dd7b3feb1bd055b95d4805
Luca Somaschini <somas99@gmail.com>
2022-07-12 17:23:54 +0200

    Pari_e_dispari: improved code

--- pari_e_dispari/js/main.js
+++ pari_e_dispari/js/main.js
@@ -1,44 +1,58 @@
 /*
+
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
+
 */
 
 const userNumber = prompt('Please, choose a number between 1 and 5');
 
-const computerNumber = generateComputerNumber();
+if (userNumber < 1 || userNumber > 5) {
 
-// funzione per generare il numbero random per il pc
-function generateComputerNumber() {
-    let randomNumber = Math.floor(Math.random() * 6 + 1);
-    console.log(randomNumber + ' funzione');
-    return randomNumber;
-}
+    alert('Errore: devi inserire un numero tra 1 e 5. Ricarica la pagina.')
 
-//debug
-console.log(computerNumber + ' fuori funzione');
+    
+} else {
 
-// somma
-const sum = parseInt(userNumber) + parseInt(computerNumber);
-console.log(sum + ' somma numeri');
+    const computerNumber = generateComputerNumber();
 
-let isSumOdd;
-
-// funzione pari o dispari
-function oddEvenFunction() {
-    if (sum % 2 == 0) {
-        isSumOdd = true;
-    } else {
-        isSumOdd = false;
+    // funzione per generare il numbero random per il pc
+    function generateComputerNumber() {
+        let randomNumber = Math.floor(Math.random() * 6 + 1);
+        console.log(randomNumber + ' funzione');
+        return randomNumber;
     }
-    return isSumOdd;
-}
 
-// condizione finale
-if (oddEvenFunction() == true) {
-    console.log('il numero è pari')
-} else if (oddEvenFunction() == false) {
-    console.log('il numero è dispari')
+    //debug
+    console.log(computerNumber + ' fuori funzione');
+
+    // somma
+    const sum = parseInt(userNumber) + parseInt(computerNumber);
+    console.log(sum + ' somma numeri');
+
+    let isSumOdd;
+
+    // funzione pari o dispari
+    function oddEvenFunction(prova11) {
+        if (prova11 % 2 == 0) {
+            isSumOdd = true;
+        } else {
+            isSumOdd = false;
+        }
+        return isSumOdd;
+    }
+
+    // lego variabile a funzione che uso sulla variabile sum
+    let result = oddEvenFunction(sum);
+
+    // condizione finale
+    if (result == true) {
+        console.log('il numero è pari')
+    } else if (result == false) {
+        console.log('il numero è dispari')
+    }
+
 }
